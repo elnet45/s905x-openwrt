db2a0fb47b9c9c21d82d2954acac925f6eedfbdb
sweetieice <andrraawrt@gmail.com>
2022-01-09 06:22:49 +0000

    openwrt: Initial import homemade edition

---
+++ router-config/homemade/diy-part2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# ------------------------------- Main source started -------------------------------
+#
+# Modify default theme（FROM luci-theme-bootstrap CHANGE TO luci-theme-material）
+sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
+
+# Modify default theme（FROM luci-theme-material CHANGE TO luci-theme-argon）
+sed -i 's/luci-theme-material/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+
+# Add autocore support for armvirt
+sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
+
+# Set etc/openwrt_release
+sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
+echo "DISTRIB_SOURCECODE='Homemade'" >>package/base-files/files/etc/openwrt_release
+
+# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.10.1）
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+#
+# ------------------------------- Main source ends -------------------------------
+
+# ------------------------------- Other started -------------------------------
+#
+
+#
+# ------------------------------- Other ends -------------------------------
+
